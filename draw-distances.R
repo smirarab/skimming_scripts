fddd06f510da8f8057b4a5be479f9616d5d61a2d
Siavash Mirarab <smirarab@ucsd.edu>
2023-01-05 13:51:14 -0800

    update script

--- draw-distances.R
+++ draw-distances.R
@@ -2,8 +2,10 @@
 args = commandArgs(trailingOnly=TRUE)
 print(args)
 m=as.matrix(read.csv(args[1],sep='\t',row.names=1)); 
-pdf(paste('fig',args[2],'.pdf',sep=''),width=12,height=9);
+pdf(paste('fig',args[2],'.pdf',sep=''),width=12,height=10);
   plot(hclust(as.dist(m))); 
-  heatmap(m, scale = 'none', col = heat.colors(128,rev=T));
+  heatmap(m, scale = 'none', col = heat.colors(100,rev=T));
+  try(require(gplots))
+  try(heatmap.2(m, scale = "none", col = heat.colors(100,rev=T), trace = "none",margin=c(16,18)))
 dev.off(); 
 write.table(file=paste('dist',args[2],'-reformatted.txt',sep=''),format(data.frame(name = row.names(m), m),digits=3),qu=F,sep='\t',row.names = FALSE);
