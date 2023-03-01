81a94d3a904c0805d308a0db6d740dba230df7e5
DeevanshuGoyal <deevanshugoyal@gmail.com>
2023-02-28 16:05:44 -0800

    Removed obsolete folder

--- Skim_processing_pipelines/Pipelines/conda_source.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-eval "$(conda shell.bash hook)"
-CONDAENV=GSkim
-conda activate $CONDAENV
--- Skim_processing_pipelines/Pipelines/jc-correction.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-dist=${1:-ref-dist-mat.txt}  
-
-cat $dist | awk 'BEGIN{OFS="\t";} (NR==1){print $0} (NR>1) {for(i=2;i<=NF;i++){$i=-3/4*log(1-4/3*$i)}; print $0}'
